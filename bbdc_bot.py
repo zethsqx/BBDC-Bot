835b56de76ce74128c7f54f1ec9dc3f3b24a709e
root <chongmingliang92@gmail.com>
2022-01-19 00:16:38 +0800

    code changes

--- bbdc_bot.py
+++ bbdc_bot.py
@@ -7,112 +7,134 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 import os
+import subprocess
+from datetime import datetime, timedelta
+import urllib.request
+import ssl
+import json
 
 server ="http://127.0.0.1:4444/wd/hub"
 
-#service = Service(executable_path="/root/BBDC-Bot/geckodriver")
-browser_options = webdriver.ChromeOptions()#service=service)
-browser_options.accept_untrusted_certs = True
-#browser_options.add_argument('--headless')
-browser = webdriver.Remote(command_executor=server,options=browser_options)
-
-#browser = webdriver.Chrome('/root/BBDC-Bot/chromedriver')
-#service = Service(executable_path=ChromeDriverManager().install())
-#browser = webdriver.Chrome(service=service,service_args=["--headless", "--no-sandbox", "--disable-dev-shm-usage"])
-#browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-# Get environment variables
+# Import env values for telegram
 USER = os.getenv('BOOKING_USER')
-PASSWORD = os.environ.get('BOOKING_PASSWORD')
+PASSWORD = os.getenv('BOOKING_PASSWORD')
+
+# Import env values for telegram
+chatid = os.getenv('chatid') #replace if hardcode needed
+teleid = os.getenv('teleid') #replace if hardcode needed
+telelink = "https://api.telegram.org/" + teleid + "/{}?{}"
+
+def broadcastMessage(elink,einfo):
+   headers = {"Accept": "application/json"}                                                                                 
+   myssl = ssl._create_unverified_context()     
+   params = {"text": einfo}
+   params.update({"chat_id": chatid})
+   url = elink.format("sendMessage", urllib.parse.urlencode(params))
+   request = urllib.request.Request(url, None, headers)
+   with urllib.request.urlopen(request, context=myssl) as r:
+     r.read()
 
 try: 
- # Load Main Login Page
- browser.get('https://info.bbdc.sg/members-login/')
- print("Main Login Page")
- 
- # Input Username, Password and Login
- idLogin = browser.find_element(By.ID, "txtNRIC")
- idLogin.send_keys(USER)
- idLogin = browser.find_element(By.ID, "txtPassword")
- idLogin.send_keys(PASSWORD)
- loginButton = browser.find_element(By.ID, "loginbtn")
- loginButton.click()
- print("Logging In")
- 
- 
- # Accept Insecure and Choose default enrolled course
- proceedBtn = browser.find_element(By.ID, 'proceed-button')
- proceedBtn.click()
- submitBtn = browser.find_element(By.NAME,'btnSubmit')
- submitBtn.click()
- print("Main Booking Page")
- 
- 
- # Switching to Left Frame and accessing element by text
- browser.switch_to.default_content()
- frame = browser.find_element(By.NAME, 'leftFrame')
- browser.switch_to.frame(frame)
- bookingBtn = browser.find_element(By.CSS_SELECTOR, "a[href*='../b-2-pLessonBooking.asp?limit=pl']");
- bookingBtn.click()
- print("Clicking Practical Booking")
- 
- 
- #ids = browser.find_elements_by_xpath('//*[@id]')
- #for ii in ids:
-     #print(ii.get_attribute('id'))
- #quit()
- 
- # Selection menu
- browser.switch_to.default_content()
- wait = WebDriverWait(browser, 300)
- wait.until(EC.frame_to_be_available_and_switch_to_it(browser.find_element(By.NAME, 'mainFrame')))
- wait.until(EC.visibility_of_element_located((By.ID, "checkMonth")))
- 
- # 0 refers to first month, 1 refers to second month, and so on...
- months = browser.find_element(By.ID, 'checkMonth')
- months[12].click() # all months
- 
- # 0 refers to first session, 1 refers to second session, and so on...
- sessions = browser.find_element(By.ID, 'checkSes')
- sessions[8].click() # all sessions
- 
- # 0 refers to first day, 1 refers to second day, and so on...
- days = browser.find_element(By.ID, 'checkDay')
- days[7].click() # all days
- 
- # Selecting Search
- browser.find_element(By.NAME, 'btnSearch').click()
- 
- # Dismissing Prompt
- wait = WebDriverWait(browser, 300)
- wait.until(EC.alert_is_present())
- alert_obj = browser.switch_to.alert
- alert_obj.accept()
- wait.until(EC.visibility_of_element_located((By.NAME, "slot")))
- 
- # 0 refers to first slot, 1 refers to second slot, and so on...
- slots = browser.find_element(By.NAME, 'slot')
- for slot in slots:     # Selecting all checkboxes
-     #slot.click()
-     timeslot = slot.find_element(By.XPATH, './..').get_attribute('onmouseover')
-     print(timeslot[20:58])
-     #browser.find_element_by_class_name('pgtitle').click()     # clicking random element to hide hover effect
- 
- #print(slots)
- quit()
- 
- #ids = browser.find_elements_by_xpath('//*[@id]')
- #for ii in ids:
-     #print(ii.get_attribute('id'))
- #quit()
- 
- # Selecting Submit
- #browser.find_element_by_name('btnSubmit').click()
- 
- # Selecting confirm
- #wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@value='Confirm']")))
- #browser.find_element_by_xpath("//input[@value='Confirm']").click()
+  browser_options = webdriver.ChromeOptions()
+  browser_options.accept_untrusted_certs = True
+  browser = webdriver.Remote(command_executor=server,options=browser_options)
 
+  # Load Main Login Page
+  browser.get('https://info.bbdc.sg/members-login/')
+  print("Main Login Page Loaded...")
+  
+  # Input Username, Password and Login
+  idLogin = browser.find_element(By.ID, "txtNRIC")
+  idLogin.send_keys(USER)
+  idLogin = browser.find_element(By.ID, "txtPassword")
+  idLogin.send_keys(PASSWORD)
+  loginButton = browser.find_element(By.ID, "loginbtn")
+  loginButton.click()
+  print("...Clicking Log In")
+   
+  # Accept Insecure and Choose default enrolled course
+  proceedBtn = browser.find_element(By.ID, 'proceed-button')
+  proceedBtn.click()
+  submitBtn = browser.find_element(By.NAME,'btnSubmit')
+  submitBtn.click()
+  print("Main Booking Page Loaded...")
+    
+  # Switching to Left Frame and accessing element by text
+  browser.switch_to.default_content()
+  frame = browser.find_element(By.NAME, 'leftFrame')
+  browser.switch_to.frame(frame)
+  bookingBtn = browser.find_element(By.CSS_SELECTOR, "a[href*='../b-2-pLessonBooking.asp?limit=pl']");
+  bookingBtn.click()
+  print("...Clicking Practical Booking")
+  
+  # Selection menu
+  browser.switch_to.default_content()
+  wait = WebDriverWait(browser, 300)
+  wait.until(EC.frame_to_be_available_and_switch_to_it(browser.find_element(By.NAME, 'mainFrame')))
+  wait.until(EC.visibility_of_element_located((By.ID, "checkMonth")))
+  print("Main Frame Page Loaded...")
+ 
+  # 0 refers to first month, 1 refers to second month, and so on...
+  months = browser.find_elements(By.ID, 'checkMonth')
+  months[12].click() # all months
+  # 0 refers to first session, 1 refers to second session, and so on...
+  sessions = browser.find_elements(By.ID, 'checkSes')
+  sessions[8].click() # all sessions
+  # 0 refers to first day, 1 refers to second day, and so on...
+  days = browser.find_elements(By.ID, 'checkDay')
+  days[7].click() # all days
+  # Selecting Search
+  browser.find_element(By.NAME, 'btnSearch').click()
+  print("...Clicking Search after checking all boxes")
+ 
+  # Dismissing Prompt
+  wait = WebDriverWait(browser, 300)
+  wait.until(EC.alert_is_present())
+  alert_obj = browser.switch_to.alert
+  alert_obj.accept()
+  wait.until(EC.visibility_of_element_located((By.NAME, "slot")))
+  print("===Dismissed Prompt===")
+
+  # 0 refers to first slot, 1 refers to second slot, and so on...
+  slots = browser.find_elements(By.NAME, 'slot')
+  timeinfoList = []
+  for slot in slots:     # Selecting all checkboxes
+      #slot.click()
+      timeslot = slot.find_element(By.XPATH, './..').get_attribute('onmouseover')
+      timeinfo = timeslot[20:58].replace('"', '').split(",")
+      timeinfoList.append(timeinfo)
+  
+  #print(timeinfoList) 
+  #timeinfoList.append(['11/01/2022 (Wed)', '6', '17:10', '18:50'])
+  #timeinfoList.append(['12/01/2022 (Wed)', '6', '17:10', '18:50'])
+  #timeinfoList.append(['30/01/2022 (Wed)', '6', '17:10', '18:50'])
+  #timeinfoList.append(['29/01/2022 (Wed)', '6', '17:10', '18:50'])  
+  
+  start_date = datetime.today()
+  end_date = start_date + timedelta(6)
+
+  printList = []
+  for t in timeinfoList: 
+    dtobj = datetime.strptime(t[0][0:10], "%d/%m/%Y")
+    if dtobj <= end_date:
+      printList.append(' '.join(t))
+ 
+  print(printList)
+
+  # Craft message and send to telegram
+  mptdata = '\n'.join(printList)
+  mptdata = 'Run @ ' + str(start_date) + '\n\n' + mptdata
+  broadcastMessage(telelink, mptdata)
+
+  subprocess.run(["podman", "restart", "chrome"]) 
+  quit()
+  
+  #ids = browser.find_elements_by_xpath('//*[@id]')
+  #for ii in ids:
+      #print(ii.get_attribute('id'))
+  #quit()
+ 
 except Exception as e:
   print(e) 
+  subprocess.run(["podman", "restart", "chrome"])
   quit()
